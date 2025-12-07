ce404b22b70e7a552aa3987ba8116b9abb6104e8
yo <paginas_01@hotmail.com>
2025-12-07 13:53:00 -0300

    ya se ven las publicaciones con el template

--- BLOG/blog/apps/posts/views.py
+++ BLOG/blog/apps/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+# Vista para la página de inicio
+def index(request):
+    # Obtener todos los posts de la base de datos
+    posts = Post.objects.all()
+    
+    # Crear el contexto (la cajita de datos que viaja al HTML)
+    ctx = {
+        'posts': posts
+    }
+    
+    # Renderizar el template con los datos
+    return render(request, 'index.html', ctx)
